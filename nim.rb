5fa06dce2a13385252aacd3ff7d080ba47a8e7bc
zhongjixiang <zjx632@gmail.com>
2015-05-04 21:59:55 +0800

    update to 0.11.0

--- nim.rb
+++ nim.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Nim < Formula
   homepage "http://nim-lang.org/"
 
-  url "http://nim-lang.org/download/nim-0.10.2.zip"
-  sha1 '0a54d6d7f257cdade5bf950d318066959c48a6dc'
-  version "0.10.2"
+  url "http://nim-lang.org/download/nim-0.11.0.tar.xz"
+  sha1 '5cbad487c94d0dd4b8cbd52486dd43b1ccf671fb'
+  version "0.11.0"
 
   def install
     system "/bin/sh", "build.sh"
